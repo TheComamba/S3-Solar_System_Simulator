6be2d11dad47c9b82139e980892ab0fc4bcc6cc9
TheComamba <thecomamba@jojoheinze.de>
2023-10-08 13:49:47 +0200

    Compiling canvas.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,4 +1,9 @@
-use iced::{widget::Row, Sandbox};
+use std::vec;
+
+use iced::{
+    widget::{canvas, Row},
+    Sandbox,
+};
 
 use crate::sim::{initial_parameters::InitialParameters, system::StellarSystem};
 
@@ -25,3 +30,35 @@ impl Sandbox for Gui {
         Row::new().into()
     }
 }
+
+struct CanvasState {
+    cache: canvas::Cache,
+    system: StellarSystem,
+}
+
+impl CanvasState {
+    fn new(system: StellarSystem) -> CanvasState {
+        CanvasState {
+            cache: canvas::Cache::default(),
+            system,
+        }
+    }
+}
+
+impl<GuiMessage> canvas::Program<GuiMessage> for CanvasState {
+    type State = ();
+
+    fn draw(
+        &self,
+        _state: &Self::State,
+        renderer: &iced::Renderer,
+        _theme: &iced::theme::Theme,
+        bounds: iced::Rectangle,
+        _cursor: iced::mouse::Cursor,
+    ) -> Vec<canvas::Geometry> {
+        let visualisation = self.cache.draw(renderer, bounds.size(), |frame| {});
+        vec![visualisation]
+    }
+}
+
+enum GuiMessage {}
